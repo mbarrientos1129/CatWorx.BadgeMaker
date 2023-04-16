e5531830af160955d10986c6437557e3221af2db
mbarrientos.webdevelopment@gmail.com <91514897+mbarrientos1129@users.noreply.github.com>
2023-04-16 14:50:54 -0400

    constructors

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+// See https://aka.ms/new-console-template for more information
+
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter name: (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+
+            return employees;
+        }
+
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+    }
+}
