a6cff4864b6c591ecd02bc0c402b7f5036ca2139
mbarrientos.webdevelopment@gmail.com <91514897+mbarrientos1129@users.noreply.github.com>
2023-04-17 18:06:04 -0400

    logic for creating folder and populating CSV file

--- Util.cs
+++ Util.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
@@ -14,5 +15,26 @@ namespace CatWorx.BadgeMaker
             }
         }
 
+        public static void MakeCSV(List<Employee> employees)
+        {
+            //Check to see if the folder exsists
+            if (!Directory.Exists("data"))
+            {
+                //If not, create it
+                Directory.CreateDirectory("data");
+            }
+
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+
+            }
+        }
     }
 }
